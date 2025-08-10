a4e7d2675aa205f26afc4431bd64ed561be9cb54
vijay <vijaykollu6@gmail.com>
2025-08-10 14:40:03 +0530

    springboot proj

---
+++ src/main/java/com/klef/SpringBootProjectApplication.java
@@ -0,0 +1,20 @@
+package com.klef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootProjectApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootProjectApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectApplication.class, args);
+        System.out.println("hello DevOps ...!!");
+    }
+}
